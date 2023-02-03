768e1ae79a40bb674912dbdca9b787a0274a599b
luisamlopez0 <lmlopez.19@est.ucab.edu.ve>
2023-02-03 01:30:22 -0400

    Add ProductCard

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="product-card">
+    <img
+      src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+      alt=""
+    />
+    <div class="product-info">
+      <div>
+        <p><slot name="name"></slot></p>
+        <p>$<slot name="price"></slot></p>
+        <p>Stock: <slot name="stock"></slot> unidades</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.product-card {
+  width: 240px;
+  box-shadow: 2px 2px 2px 1px var(--box-shadow);
+  border-radius: 4px;
+  border: 2px solid var(--vt-c-divider-dark-1);
+}
+
+.product-card img {
+  width: 236px;
+  height: 236px;
+  border-radius: 4px;
+  object-fit: cover;
+}
+
+.product-info {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px;
+}
+
+.product-info div p:nth-child(1) {
+  font-weight: bold;
+  font-size: var(--md);
+}
+
+.product-info figure {
+  margin: 0;
+}
+
+.product-info figure img {
+  width: 35px;
+  height: 35px;
+  margin-top: 0;
+  margin-bottom: 4px;
+}
+
+.product-info div p:nth-child(2) {
+  font-size: var(--sm);
+  margin-top: 0;
+  margin-bottom: 0;
+  color: var(--very-light-pink);
+}
+</style>
--- src/views/CodeView.vue
+++ src/views/CodeView.vue
@@ -1,13 +1,71 @@
+<script setup>
+import ProductCard from "../components/ProductCard.vue";
+</script>
+
 <template>
-  <div class="code">
+  <div>
     <h1>Ejemplo de componentes simulando inventario</h1>
+
+    <div class="cards-container">
+      <ProductCard>
+        <template #name> Producto 1 </template>
+        <template #price> 100 </template>
+        <template #stock> 10 </template>
+      </ProductCard>
+      <ProductCard>
+        <template #name> Producto 2 </template>
+        <template #price> 200 </template>
+        <template #stock> 20 </template>
+      </ProductCard>
+      <ProductCard>
+        <template #name> Producto 3 </template>
+        <template #price> 300 </template>
+        <template #stock> 30 </template>
+      </ProductCard>
+      <ProductCard>
+        <template #name> Producto 4 </template>
+        <template #price> 400 </template>
+        <template #stock> 40 </template>
+      </ProductCard>
+      <ProductCard>
+        <template #name> Producto 5 </template>
+        <template #price> 500 </template>
+        <template #stock> 50 </template>
+      </ProductCard>
+      <ProductCard>
+        <template #name> Producto 6 </template>
+        <template #price> 600 </template>
+        <template #stock> 60 </template>
+      </ProductCard>
+      <ProductCard>
+        <template #name> Producto 7 </template>
+        <template #price> 700 </template>
+        <template #stock> 70 </template>
+      </ProductCard>
+      <ProductCard>
+        <template #name> Producto 8 </template>
+        <template #price> 800 </template>
+        <template #stock> 80 </template>
+      </ProductCard>
+    </div>
   </div>
 </template>
 
 <style>
+h1 {
+  text-align: center;
+  margin-bottom: 2rem;
+}
+.cards-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 240px);
+  gap: 26px;
+  place-content: center;
+  margin: 0 auto;
+  margin-bottom: 2rem;
+}
 @media (min-width: 1024px) {
   .code {
-    min-height: 100vh;
     display: flex;
     align-items: center;
   }
